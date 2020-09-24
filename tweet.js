1663f8560785c7a9d343cdca6bebc09023d71f8a
Joe Webster <joseph.scott.webster@gmail.com>
2020-09-24 15:43:13 -0400

    Write code to pass tests.

--- tweet.js
+++ tweet.js
@@ -1,5 +1,25 @@
 class Tweet {
+  constructor(sender, content) {
+    this.sender = sender;
+    this.content = content;
+    this.likes = 0;
+    this.retweets = 0;
+    this.replies = [];
+    this.replyCount = 0;
+  }
 
+  like() {
+    this.likes++;
+  }
+
+  retweet() {
+    this.retweets++;
+  }
+
+  reply(message) {
+    this.replies.push(message);
+    this.replyCount++;
+  }
 }
 
 module.exports = Tweet;
